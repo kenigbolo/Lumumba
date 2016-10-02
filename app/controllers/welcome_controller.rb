dd6becd708f4c2af1e516918ea4be093d2ab7704
kenigbolo <kenigbol@ut.ee>
2016-10-02 19:18:04 +0300

    Raise 404 for invalid profile request

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,9 +7,11 @@ class WelcomeController < ApplicationController
   	if session["current_user_id"] != nil
   	  @user = User.where("id = ?", session["current_user_id"])[0]
   	  @designs = Design.where("user_id = ?", session["current_user_id"])
-  	else
+  	elsif params[:id] != nil
   	  @user = User.where("id = ?", params[:id])[0]
   	  @designs = Design.where("user_id = ?", params[:id])
+  	else
+  	  render :file => 'public/404.html', :status => :not_found, :layout => false
   	end
   end
 end
