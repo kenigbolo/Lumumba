0639637133abcb1ff60e5eb9a578388816b8243b
kenigbolo <kenigbol@ut.ee>
2016-10-03 12:45:34 +0300

    Modify controller to handle devise login user_id

--- app/controllers/design_controller.rb
+++ app/controllers/design_controller.rb
@@ -6,15 +6,29 @@ class DesignController < ApplicationController
   end
 
   def create
-  	design = Design.create(name: params[:design][:name], 
-  		description: params[:design][:description], user_id: session["current_user_id"],
-  		image: params[:design][:image])
+  	if session["current_user_id"] != nil
+	  design = Design.create(name: params[:design][:name], 
+	  	description: params[:design][:description], user_id: session["current_user_id"],
+	  	image: params[:design][:image])
+
+  	elsif session["warden.user.user.key"][0][0] != nil
+	  design = Design.create(name: params[:design][:name], 
+	  	description: params[:design][:description], user_id: session["warden.user.user.key"][0][0],
+	  	image: params[:design][:image]) 
+
+  	end
+
   	redirect_to user_profile_path_url
   end
 
   def delete
-  	design = Design.where("user_id = ?", session["current_user_id"]).where("id = ?", params[:id]).first
-    design.destroy
+  	if session["current_user_id"] != nil
+  	  design = Design.where("user_id = ?", session["current_user_id"]).where("id = ?", params[:id]).first
+      design.destroy
+    elsif session["warden.user.user.key"][0][0] != nil
+  	  design = Design.where("user_id = ?", session["warden.user.user.key"][0][0]).where("id = ?", params[:id]).first
+      design.destroy
+  	end
     redirect_to user_profile_path_url
   end
 
